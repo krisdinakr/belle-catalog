9fee190dea0d8238682f8b8c42c3f8a2f42b20d8
Krisdina <krssdina@gmail.com>
2024-01-24 11:35:38 +0700

    add user and verification services

---
+++ src/services/userService.ts
@@ -0,0 +1,74 @@
+import { ClientSession, ObjectId } from 'mongoose'
+
+import { User } from '@/models'
+
+export const userService = {
+  create: (
+    {
+      email,
+      password,
+      verified = false
+    }: {
+      email: string
+      password: string
+      verified?: boolean
+    },
+    session?: ClientSession
+  ) =>
+    new User({
+      email,
+      password,
+      verified
+    }).save({ session }),
+
+  getById: (userId: ObjectId) => User.findById(userId),
+
+  getByEmail: (email: string) => User.findOne({ email }),
+
+  isExistByEmail: (email: string) => User.exists({ email }),
+
+  updatePasswordByUserId: (
+    userId: ObjectId,
+    password: string,
+    session?: ClientSession
+  ) => {
+    const data = [{ _id: userId }, { password, resetPasswords: [] }]
+
+    let params = null
+
+    if (session) {
+      params = [...data, { session }]
+    } else {
+      params = data
+    }
+
+    return User.updateOne(...params)
+  },
+
+  addVerificationToUser: async (
+    {
+      userId,
+      verificationId
+    }: {
+      userId: ObjectId
+      verificationId: ObjectId
+    },
+    session?: ClientSession
+  ) => {
+    let options = {}
+
+    if (session) {
+      options = { session }
+    }
+
+    const user = await User.findOne({ _id: userId }, null, options)
+
+    if (user) {
+      if (!user.verifications) {
+        user.verifications = []
+      }
+      user.verifications.push(verificationId)
+      await user.save({ session })
+    }
+  }
+}
